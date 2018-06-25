526b8dcce13cbb42c3e4fc81e15abebbac72f9c4
Diego Avelar <diegoarmando2011@gmail.com>
2018-06-25 11:58:14 -0300

    adding word github

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require 'vendor/autoload.php';
 $client = new Github(new Client());
 $repositories = $client->listRepositories('symfony');
 
-echo '<h3>Symfony Repositories from</h3>';
+echo '<h3>Symfony Repositories from Github</h3>';
 foreach($repositories as $repository) {
     echo "<b>{$repository['name']}</b><br>";
     echo $repository['description'] . '<br><br>';
